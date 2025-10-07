d32503f18b0f8992ba96fd084488b9d1f0382816
@m.giella4 <m.giella4@studenti.unisa.it>
2025-10-07 16:41:12 +0200

    Add reduce function to parallel sum

--- parallel_sum.c
+++ parallel_sum.c
@@ -6,10 +6,32 @@
 #define max_iterations 5
 #define scale_times 5
 
+
+void reduce(double *sum, int rank, int world) {
+    int step = 2;
+    int distance = 1; 
+    while (distance < world) {
+        if ((rank % step) != 0) {
+                MPI_Send(sum, 1, MPI_DOUBLE, rank-distance, 0, MPI_COMM_WORLD);
+                break; // chi manda esce
+        } else {
+            double received = 0.0;
+            MPI_Recv(&received, 1, MPI_DOUBLE, rank+distance,0 , MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            *sum += received;
+           }
+            distance=step;
+            step*=2;
+        }
+    
+        
+}
+
+
+
 int main(int argc, char** argv) {
     // Initialize the MPI environment
     MPI_Init(NULL, NULL);
-    srand((unsigned int) time(0)); 
+    srand((double) time(0)); 
     float start,end;
     int n=scalable_n;
 
@@ -24,43 +46,42 @@ int main(int argc, char** argv) {
     //scale input n "scale_times" times
     for(int scale=0;scale<scale_times;scale++){
 
-        int* sum_array = calloc(n,sizeof(int));
-        int* recv_array = calloc(n/world_size,sizeof(int));
+        double* sum_array = calloc(n,sizeof(double));
+        double* recv_array = calloc(n/world_size,sizeof(double));
         float total_mean;
         float mean=0;
-        int total_sum;
+
         //for each input value repeat executions "max_iterations" times to obtain mean
         for(int repeat=0;repeat<max_iterations;repeat++){
-            int sum=0;
             if(rank==0){
                 for(int i=0;i<n;i++){
-                    sum_array[i]=rand()%3;
+                    sum_array[i]=(rand()%3)/10.0;
                 } 
             }
-
+            double sum=0;
+            MPI_Barrier(MPI_COMM_WORLD);
 			start=MPI_Wtime()*1000; //conversion in ms
 
             //sends portions of array to all procs
-            MPI_Scatter(sum_array, n/world_size, MPI_INT, recv_array, n/world_size, MPI_INT, 0,MPI_COMM_WORLD);
+            MPI_Scatter(sum_array, n/world_size, MPI_DOUBLE, recv_array, n/world_size, MPI_DOUBLE, 0,MPI_COMM_WORLD);
 
-            MPI_Barrier(MPI_COMM_WORLD);
             
             for(int i=0;i<n/world_size;i++){
                 sum+=recv_array[i];
             }
-
-            MPI_Reduce(&sum, &total_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+            printf("sum %.2f\n\n",sum);
+            reduce(&sum,rank,world_size); //all procs reduce sum to proc 0
             MPI_Barrier(MPI_COMM_WORLD);
             end=MPI_Wtime()*1000-start;
         	mean+=end;
+            
         }
 
         //print mean time of execution 
         MPI_Reduce(&mean, &total_mean, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
 
         if(rank==0){
-            printf("total sum is %d\n",total_sum);
-            printf("Mean execution time with %d processes, %d iterations and n=%d: %.3f\n\n",world_size,max_iterations,n,total_mean/max_iterations);    
+            printf("Mean execution time with %d processes, %d iterations and n=%d: %.2f\n\n",world_size,max_iterations,n,total_mean/max_iterations);    
         }
         n*=2;    
         free(sum_array);
@@ -70,3 +91,4 @@ int main(int argc, char** argv) {
    
     MPI_Finalize();
 }
+
